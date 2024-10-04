717fa30fc35f9fc83e48468cccbe70d995621601
Mitchell McKee <mckee.m@northeastern.edu>
2024-10-04 02:51:26 -0400

    removed old mock data and added typing

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import requests
 import json
-import csv
+from typing import Union, Dict, Any
 
 app = Flask(__name__)
 
@@ -10,34 +10,33 @@ topdeck_auth_header = {
     'Authorization': '27d14d9f-b081-44d8-8dfc-2c9ff9384712'
 }
 topdeck_req_body = {
-    'last': 5,
+    'last': 180,
     "columns": ["decklist", "wins", "draws", "losses"],
     'game': 'Magic: The Gathering',
     'format': 'EDH',
-    "participantMin": 24,
-    "participantMax": 32
+    "participantMin": 16
 }
 
 moxfield_base_url = "https://api2.moxfield.com/"
 mox_path = "v3/decks/all/"
 
-class Commander:
-    def __init__(self, commanders, card_name, moxfield_deck_id):
-        self.commanders = commanders
+class CardAndCommander:
+    def __init__(self, card_name: str, commanders: list[str], moxfield_deck_id: str):
         self.card_name = card_name
+        self.commanders = commanders
         self.moxfield_deck_id = moxfield_deck_id
 
     def __repr__(self):
         return f"Deck(commanders={self.commanders}, cardName='{self.card_name}', moxfieldDeckId='{self.moxfield_deck_id}')"
 
-def fetch_topdeck_lists():
+def fetch_topdeck_lists() -> Union[None, Dict[str, Any]]:
     response = requests.post(topdeck_base_url + '/v2/tournaments', json=topdeck_req_body, headers=topdeck_auth_header)
     if response.status_code == 200:
         return response.json()
     else:
         return None
     
-def fetch_moxfield_list(deckId):
+def fetch_moxfield_list(deckId) ->  Union[None, Dict[str, Any]]:
     response = requests.get(moxfield_base_url + mox_path + deckId)
     if response.status_code == 200:
         return response.json()
@@ -94,14 +93,15 @@ def extract_moxfield_info(moxfield_list, id):
         mainboard_card_names = find_values_by_key(mainboard, "name")  
         if len(mainboard_card_names) != 0:
             for card in mainboard_card_names:
-                commander_obj = Commander(commander_card_names, card, id)
+                commander_obj = CardAndCommander(card, commander_card_names, id)
                 result.append(commander_obj)
     return result
 
-def map_commanders_to_cards(data):
+# takes
+def map_commanders_to_cards(data: list[CardAndCommander]) -> Dict:
     result = {}
     for entry in data:
-        # Get the cardName and moxfieldDeckId from the entry
+        # Get the card name and moxfield deck id from the entry
         card_name = entry.card_name
         moxfield_deck_id = entry.moxfield_deck_id
         
@@ -111,14 +111,20 @@ def map_commanders_to_cards(data):
         if(len(commander_arr) > 1):
             commander = "+".join(commander_arr)
         else:
-            commander = commander_arr[0]
+            try:
+                commander = commander_arr[0]
+            except:
+                commander = 'error'
+                print('error')
+                print(entry)
         
         # If the commander is not in the result, initialize with an empty list
-        if commander not in result:
+        if commander not in result and len(commander) != 0:
             result[commander] = []
         
         # Append the tuple (cardName, moxfieldDeckId) to the list for this commander
         result[commander].append((card_name, moxfield_deck_id))
+        
     return result
 
 def get_unique_second_values(tuples_list):
@@ -157,13 +163,18 @@ def home():
                 print(index)
                 print(len(extract_ids))
 
+        print('map cmdrs')
+        print(moxfield_lists[:3])
         commander_list = map_commanders_to_cards(moxfield_lists)
         result = {}
         for commander_object in commander_list.items():
             print(commander_object)
             result[commander_object[0]] = get_unique_second_values(commander_object[1])
+            # split clause is to handle flip commanders with '//' in their name
+            commander_filename = "./output/" + commander_object[0].split(' /')[0]
+            write_to_json(result[commander_object[0]], commander_filename)
              
-        write_to_json(result, 'output')
+        write_to_json(result, './output/output')
         return result
     else:
         return jsonify({'error': 'Failed to fetch data'}), 500
