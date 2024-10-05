36c8e575770ff21b5a5c8f37dfd7007a9e40aabe
Mitchell McKee <mckee.m@northeastern.edu>
2024-10-05 17:04:09 -0400

    Filter now returns dict of Card Objects

--- filter.py
+++ filter.py
@@ -17,11 +17,75 @@ def write_to_json(data, filename):
     with open(json_file_path, mode='w') as file:
         json.dump(data, file, indent=4)
 
+def create_card_list(data):
+    result = []
+    all_image_links = get_all_image_links()
+    for key, value_list in data.items():
+        image_link = all_image_links[key]
+        if isinstance(image_link, str):
+            image_link = [image_link]
+        total_entries = get_total_entries(key)
+        new_card = {
+            'id': len(result),
+            'card': [key],
+            'relatedItems': value_list,
+            'imageLinks': image_link,
+            'totalEntries': total_entries
+        }
+        result.append(new_card)
+    
+    return result
+
+with open('./bulk/oracle-cards.json', 'r') as file:
+    all_cards = json.load(file)
+
+def get_all_image_links():
+    result = {}
+    for obj in all_cards:
+        # Extract the name as the key and the 'normal' field from image_uris as the value
+        if 'name' in obj and 'image_uris' in obj and 'normal' in obj['image_uris']:
+            result[obj['name']] = obj['image_uris']['normal']
+        else:
+            try:
+                result[obj['name']] = obj['card_faces'][0]['image_uris']['normal'], obj['card_faces'][1]['image_uris']['normal']
+            except:
+                print('Failed to retrieve image link for ' + obj['name'])
+
+    return result
+
+
+
+with open('./bulk/output.json', 'r') as file:
+    all_decklists = json.load(file)
+
+def get_total_entries(card_name):
+    decklists = find_values_by_key(all_decklists, card_name)
+    count = 0
+    for deck in decklists:
+        count += len(deck) 
+    return count
+
+# finds all values for a given key of 
+def find_values_by_key(obj, key_name):
+    values = []
+    
+    # Recursive function to find values by key
+    def search_dict(d):
+        for key, value in d.items():
+            if key == key_name:
+                values.append(value)
+            if isinstance(value, dict):
+                search_dict(value)  # Recursive call for nested dictionaries
+
+    search_dict(obj)
+    return values
+
 # Open the JSON file and load its data into a Python object
-with open('./mocks/Talion, the Kindly Lord.json', 'r') as file:
+with open('./mocks/Jeska, Thrice Reborn+Tymna the Weaver.json', 'r') as file:
     data = json.load(file)
 
 filtered_data = filter_lists_by_length(data)
-pretty_json = json.dumps(filtered_data, indent=4)
-print(pretty_json)
-write_to_json(filtered_data, './mocks/Filtered Talion, the Kindly Lord.json')
+card_object_list = create_card_list(filtered_data)
+pretty_list = json.dumps(card_object_list, indent=4)
+print(pretty_list)
+write_to_json(card_object_list, './mocks/Jeska, Thrice Reborn+Tymna the Weaver')
