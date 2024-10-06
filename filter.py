589a52779b5ab4363e4a4a4cf8be2bc2acad0610
Mitchell McKee <mckee.m@northeastern.edu>
2024-10-05 22:13:33 -0400

    added filtering for home page data

--- filter.py
+++ filter.py
@@ -1,14 +1,11 @@
 import json
 import os
 
-def filter_lists_by_length(input_dict):
-    # Create a new dictionary to store the filtered data
+def filter_lists_by_length(input_dict, limit):
     result_dict = {}
     
-    # Iterate through each key and value in the input dictionary
     for key, value_list in input_dict.items():
-        # Only keep lists with fewer than 6 items
-        if len(value_list) < 6:
+        if len(value_list) < limit:
             result_dict[key] = value_list
     
     return result_dict
@@ -20,7 +17,6 @@ def write_to_json(data, filename):
 
 def create_card_list(data):
     result = []
-    all_image_links = get_all_image_links()
     for key, value_list in data.items():
         image_link = all_image_links[key]
         if isinstance(image_link, str):
@@ -37,13 +33,10 @@ def create_card_list(data):
     
     return result
 
-with open('./bulk/oracle-cards.json', 'r') as file:
-    all_cards = json.load(file)
-
 def get_all_image_links():
     result = {}
     for obj in all_cards:
-        # Extract the name as the key and the 'normal' field from image_uris as the value
+        # Check if Card has 2 faces
         if 'name' in obj and 'image_uris' in obj and 'border_crop' in obj['image_uris']:
             result[obj['name']] = obj['image_uris']['border_crop']
         else:
@@ -53,11 +46,6 @@ def get_all_image_links():
                 print('Failed to retrieve image link for ' + obj['name'])
 
     return result
-
-
-
-with open('./bulk/output.json', 'r') as file:
-    all_decklists = json.load(file)
 
 def get_total_entries(card_name):
     decklists = find_values_by_key(all_decklists, card_name)
@@ -81,20 +69,27 @@ def find_values_by_key(obj, key_name):
     search_dict(obj)
     return values
 
+with open('./bulk/oracle-cards.json', 'r') as file:
+    all_cards = json.load(file)
+with open('./bulk/output.json', 'r') as file:
+    all_decklists = json.load(file)
+
+all_image_links = get_all_image_links()
+
 dircetory_path = './mocks/raw'
-
 filenames = []
-
 try:
     names = os.listdir(dircetory_path)
     filenames = names
 except:
     print(f"'{dircetory_path}' does not exist")
 
+commander_names = []
 for file in filenames:
     with open(f"./mocks/raw/{file}", 'r') as file:
         data = json.load(file)
-    filtered_data = filter_lists_by_length(data)
+    count = max(len(value) for value in data.values())
+    filtered_data = filter_lists_by_length(data, 6)
     card_object_list = create_card_list(filtered_data)
     
     # print the data in a readable format in the console 
@@ -103,5 +98,46 @@ for file in filenames:
 
     # remove file path and json extension from the name
     name_of_file = file.name[12:].split('.json')[0]
-    write_to_json(card_object_list, f"./mocks/{name_of_file}")
-    print(f"./mocks/{name_of_file}")
+    #write_to_json(card_object_list, f"./mocks/{name_of_file}")
+    print(name_of_file)
+    print(count)
+    commander_with_count = {
+        'name': name_of_file,
+        'count': count,
+        'related': filter_lists_by_length(data, 2)
+    }
+    commander_names.append(commander_with_count)
+
+result = []
+for commander in commander_names:
+    cmdr = commander['name']
+    if cmdr == 'Etali, Primal Conqueror':
+        cmdr = 'Etali, Primal Conqueror // Etali, Primal Sickness'
+
+    if '+' in cmdr:
+        partners = cmdr.split('+')
+        image_link = [all_image_links[partners[0]], all_image_links[partners[1]]]
+    else:
+        image_link = all_image_links[cmdr]
+    
+    if isinstance(image_link, str):
+        image_link = [image_link]
+
+    total_entries = commander['count']
+    related_dict = commander['related']
+    related_items = []
+    for related_card in related_dict.keys():
+        related_items.append(related_card)
+    
+    new_card = {
+        'id': len(result),
+        'card': [cmdr],
+        'relatedItems': related_items,
+        'imageLinks': image_link,
+        'totalEntries': total_entries
+    }
+    result.append(new_card)
+
+pretty_result = json.dumps(result, indent=4)
+print(pretty_result)
+write_to_json(result, f"./mocks/commanders")
